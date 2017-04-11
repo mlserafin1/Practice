3994ce1e0c6b33dba59cd0c6997eface40b6d428
mlserafin1 <mlserafin@hotmail.com>
2017-04-10 21:01:22 -0400

    Added no evens contigency.

--- AuditionProblem.js
+++ AuditionProblem.js
@@ -15,6 +15,18 @@ function displayEvens(){
 					alert("Step value must be a number.");
 					return false;
 				}
+				if (increment <= 0){
+					alert("Step value must be a positive number.");
+					return false;
+				}
+				if (stop <= begin){
+					alert("Ending value must be greater than the starting value.");
+					return false;
+				}
+				if (begin%2!=0 && increment%2==0){
+					alert("No even numbers found.");
+					return false;
+				}
 					for (var i = begin;i <= stop;i=i + increment){
 						if (i%2 == 0){
 							result.push(i);
